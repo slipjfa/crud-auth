e3d14429f9f893be2762b731fe775b65fe5a76b0
j_fa <slipjfa@gmail.com>
2022-12-09 01:46:15 -0300

    working on custom auth, forms partially validated

--- pages/login.js
+++ pages/login.js
@@ -5,17 +5,33 @@ import styles from "../styles/Form.module.css";
 import Image from "next/image";
 import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
 import { useState } from "react";
-import { signIn } from "next-auth/react"
+import { signIn } from "next-auth/react";
+import { useFormik } from "formik";
+import login_validate from "../lib/validate";
 
 export default function Login() {
-  const [show, setShow] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: login_validate,
+    onSubmit,
+  });
+
+  async function onSubmit(values) {
+    console.log(values);
+  }
+
   async function handleGoogleSignIn() {
-    signIn('google', { callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })
+    signIn("google", { callbackUrl: process.env.NEXT_PUBLIC_BASE_URL });
   }
 
   async function handleGithubSignIn() {
-    signIn('github', { callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })
+    signIn("github", { callbackUrl: process.env.NEXT_PUBLIC_BASE_URL });
   }
+
+  const [show, setShow] = useState(false);
 
   return (
     <Layout>
@@ -25,31 +41,39 @@ export default function Login() {
 
       <section className="w-3/4 mx-auto flex flex-col gap-6">
         <div className="title">
-          <h1 className="text-gray-800 text-4xl font-bold py-2">Explore</h1>
-          <p className="w-4/5 mx-auto text-gray-400 text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          <h1 className="text-gray-800 text-4xl dark:text-gray-200 font-bold">
+            Explore
+          </h1>
+          <p className="w-4/5 mx-auto text-gray-400 dark:text-gray-400 text-sm">
+            Take a look around. It works!
           </p>
         </div>
 
-        {/* form */}
-        <form className="flex flex-col gap-2">
+        <form className="flex flex-col gap-2" onSubmit={formik.handleSubmit}>
           <div className={styles.input_group}>
             <input
               type="email"
               name="email"
               placeholder="Email"
               className={styles.input_text}
+              {...formik.getFieldProps("email")}
             />
             <span className="incon flex items-center px-4">
               <HiAtSymbol size={25} />
             </span>
           </div>
+          {formik.errors.email && formik.touched.email ? (
+            <span className="text-xs text-rose-500">{formik.errors.email}</span>
+          ) : (
+            <> </>
+          )}
           <div className={styles.input_group}>
             <input
               type={`${show ? "text" : "password"}`}
               name="password"
               placeholder="Password"
               className={styles.input_text}
+              {...formik.getFieldProps("password")}
             />
             <span
               className="incon flex items-center px-4"
@@ -58,28 +82,51 @@ export default function Login() {
               <HiFingerPrint size={25} />
             </span>
           </div>
+          {formik.errors.password && formik.touched.password ? (
+            <span className="text-xs text-rose-500">
+              {formik.errors.password}
+            </span>
+          ) : (
+            <> </>
+          )}
 
-          {/* login buttons */}
           <div className="input-button">
             <button type="submit" className={styles.button}>
               Login
             </button>
           </div>
           <div className="input-button">
-            <button type="button" onClick={handleGoogleSignIn} className={styles.button_custom}>
+            <button
+              type="button"
+              onClick={handleGoogleSignIn}
+              className={styles.button_custom}
+            >
               Sign In with Google{" "}
-              <Image src={"/assets/google.svg"} width={20} height={20} alt="google"></Image>
+              <Image
+                src={"/assets/google.svg"}
+                width={20}
+                height={20}
+                alt="google"
+              ></Image>
             </button>
           </div>
           <div className="input-button">
-            <button type="button" onClick={handleGithubSignIn} className={styles.button_custom}>
+            <button
+              type="button"
+              onClick={handleGithubSignIn}
+              className={styles.button_custom}
+            >
               Sign In with Github{" "}
-              <Image src={"/assets/github.svg"} width={25} height={25} alt="github"></Image>
+              <Image
+                src={"/assets/github.svg"}
+                width={25}
+                height={25}
+                alt="github"
+              ></Image>
             </button>
           </div>
         </form>
 
-        {/* bottom */}
         <p className="text-center text-gray-500 text-sm">
           No account?{" "}
           <Link href={"/register"} className="text-blue-700">
--- pages/register.js
+++ pages/register.js
@@ -5,87 +5,133 @@ import styles from "../styles/Form.module.css";
 import Image from "next/image";
 import { HiAtSymbol, HiFingerPrint, HiOutlineUser } from "react-icons/hi";
 import { useState } from "react";
+import { useFormik } from "formik";
+import { registerValidate } from "../lib/validate";
 
 export default function Register() {
-  const [show, setShow] = useState({ password: false, cpassword: false});
+  const [show, setShow] = useState({ password: false, cpassword: false });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      cpassword: "",
+    },
+    validate: registerValidate,
+    onSubmit,
+  });
+
+  async function onSubmit(values) {
+    console.log(values);
+  }
+
   return (
     <Layout>
       <Head>
         <title>Register</title>
       </Head>
-      <section className="w-3/4 mx-auto flex flex-col gap-10">
+      <section className="w-3/4 mx-auto flex flex-col gap-2">
         <div className="title">
-          <h1 className="text-gray-800 text-4xl font-bold py-2">Register</h1>
-          <p className="w-4/5 mx-auto text-gray-400 text-xs">
-            Lorem ipsum dolor sit amet
-          </p>
+          <h1 className="text-gray-800 text-4xl font-bold ">Register</h1>
+          <p className="w-4/5 mx-auto mt-1 text-gray-400 text-xs">Get in!</p>
         </div>
 
-        {/* form */}
-        <form className="flex flex-col gap-2">
+        <form className="flex flex-col gap-2" onSubmit={formik.handleSubmit}>
           <div className={styles.input_group}>
             <input
               type="text"
               name="Username"
               placeholder="Username"
               className={styles.input_text}
+              {...formik.getFieldProps("username")}
             />
             <span className="incon flex items-center px-4">
               <HiOutlineUser size={25} />
             </span>
           </div>
+          {formik.errors.username && formik.touched.username ? (
+            <span className="text-xs text-rose-500">
+              {formik.errors.username}
+            </span>
+          ) : (
+            <> </>
+          )}
+
           <div className={styles.input_group}>
             <input
               type="email"
               name="email"
               placeholder="Email"
               className={styles.input_text}
+              {...formik.getFieldProps("email")}
             />
             <span className="incon flex items-center px-4">
               <HiAtSymbol size={25} />
             </span>
           </div>
+          {formik.errors.email && formik.touched.email ? (
+            <span className="text-xs text-rose-500">{formik.errors.email}</span>
+          ) : (
+            <> </>
+          )}
+
           <div className={styles.input_group}>
             <input
               type={`${show.password ? "text" : "password"}`}
               name="password"
               placeholder="Password"
               className={styles.input_text}
+              {...formik.getFieldProps("password")}
             />
             <span
               className="incon flex items-center px-4"
-              onClick={() => setShow({ ...show, password: !show.password})}
+              onClick={() => setShow({ ...show, password: !show.password })}
             >
               <HiFingerPrint size={25} />
             </span>
           </div>
+          {formik.errors.password && formik.touched.password ? (
+            <span className="text-xs text-rose-500">
+              {formik.errors.password}
+            </span>
+          ) : (
+            <> </>
+          )}
+
           <div className={styles.input_group}>
             <input
               type={`${show.cpassword ? "text" : "password"}`}
               name="cpassword"
               placeholder="Confirm Password"
               className={styles.input_text}
+              {...formik.getFieldProps("cpassword")}
             />
             <span
               className="incon flex items-center px-4"
-              onClick={() => setShow({ ...show, cpassword: !show.cpassword})}
+              onClick={() => setShow({ ...show, cpassword: !show.cpassword })}
             >
               <HiFingerPrint size={25} />
             </span>
           </div>
+          {formik.errors.cpassword && formik.touched.cpassword ? (
+            <span className="text-xs text-rose-500">
+              {formik.errors.cpassword}
+            </span>
+          ) : (
+            <> </>
+          )}
 
-          {/* login buttons */}
           <div className="input-button">
             <button type="submit" className={styles.button}>
-              Login
+              Sign Up
             </button>
           </div>
         </form>
-  
-        <p className="text-center text-gray-500 text-sm">
+
+        <p className="text-center text-gray-500 text-xs">
           Already have an account?{" "}
           <Link href={"/login"} className="text-blue-700">
-            Sign in
+            Sign In
           </Link>
         </p>
       </section>
